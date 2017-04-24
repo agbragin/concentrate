b2d0b1357a88ed43b2cf8292d1f6adced9dc7e80
Alexander Afanasyev <aafanasyev@parseq.pro>
2017-04-24 17:06:40 +0300

    Merge pull request #25 in CONC/concentrate from development to master
    Squashed commit of the following:
    
    commit 69d70b252a2dfa979c07b4970003820dae66b957
    Author: Alexander Afanasyev <aafanasyev@parseq.pro>
    Date:   Mon Apr 24 15:56:21 2017 +0300
    
        VF-383 VF-490 #resolve License added; README.md updated
    
    commit 8146dfe7f4a073e91894a96fb2bedca9dab14950
    Author: Alexander Afanasyev <aafanasyev@parseq.pro>
    Date:   Mon Apr 24 11:59:54 2017 +0300
    
        Merge pull request #24 in CONC/concentrate from bugfix/VF-487-reference-change to development
    
        Squashed commit of the following:
    
        commit 1432f1fcb673f6ef704d2f0f6f1c813c599f641e
        Author: Alexander Afanasyev <aafanasyev@parseq.pro>
        Date:   Mon Apr 24 11:59:12 2017 +0300
    
            VF-413 VF-487 #resolve #comment Made Concentrate 0-based again; Striper update forced on reference change

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Welcome to gHop web-client repository page
+# Concentrate
+
+This is a client side counterpart of Concentrate.
+Taken together with [Solvent](https://github.com/parseq/solvent) they form a client-server system for genomic data visualization and filtering.
+
+Please refer to [Solvent](https://github.com/parseq/solvent) README.md to get the information about its requirements, build process and how to run the application.
--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ *     Copyright 2016-2017 the original author or authors.
+ *
+ *     This file is part of CONC.
+ *
+ *     CONC. is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     CONC. is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with CONC. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
 angular.module('ghop-ui')
 .controller('MainController', 
     ['$rootScope', '$scope', '$log', '$uibModal', '$exceptionHandler', 'AsyncService', 'TrackService', 'DataSourceService', 
@@ -176,7 +195,9 @@ angular.module('ghop-ui')
                     $log.debug(`Found ${$scope.contigsMapping[$scope.genome].length} contigs for ${$scope.genome}: ${$scope.contigsMapping[$scope.genome]}`);
 
                     $scope.contig = $scope.contigsMapping[$scope.genome][0];
-                    $scope.coord = 1;
+                    $scope.coord = 0;
+
+                    $scope.updateStriper();
                 }
             });
 
